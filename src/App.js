1460c9bcd888a3dd4e579df1b609e01e85531e63
SEUNGMIN KIM <smk950226@naver.com>
2018-09-25 12:34:35 +0900

    attribute and mixin

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styled, { injectGlobal, keyframes } from 'styled-components';
+import styled, { injectGlobal,css } from 'styled-components';
 
 injectGlobal`
   body{
@@ -7,56 +7,35 @@ injectGlobal`
     margin: 0;
   }
 `
+//card를 사용할 떄마다 쓰고싶은 스타일
+const awsomeCard = css`
+  box-shadow: 0 4px 6px rgba(50,50,50,0.11), 0 1px 3px rgba(0,0,0,0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
+`
 
 class App extends Component {
   render() {
     return (
     <Container>
-      <Button>Hello</Button>
-      <Button danger rotationTime={5}>Hello!!</Button>
-      <Anchor href="https://www.naver.com/">Go GO GO</Anchor>
+      <Input />
     </Container>
     );
   }
 }
 
+const Input = styled.input.attrs({
+  required: true
+})`
+  border: none;
+  ${awsomeCard};
+`
+
 const Container = styled.div`
   height: 100vh;
   width: 100%;
   background-color: #bdc3c7;
-`
-
-const Button = styled.button`
-  border-radius: 50px;
-  padding: 5px;
-  min-width: 120px;
-  color: #fff;
-  font-weight: 600;
-  -webkit-appearance: none;
-  cursor: pointer;
-  &:active,
-  &:focus{
-    outline: none;
-  }
-  background-color: ${props => props.danger ? "#e74c3c" : "#2ecc71"}
-  ${props => {
-    if(props.danger){
-      return `animation: ${rotation} ${props.rotationTime}s linear infinite;`
-    }
-  }}
-`
-
-const Anchor = Button.withComponent('a').extend`
-  text-decoration: none;
-`;
-
-const rotation = keyframes`
-  from{
-    transform: rotate(0deg);
-  }
-  to{
-    transform: rotate(360deg);
-  }
 `
 
 export default App;
